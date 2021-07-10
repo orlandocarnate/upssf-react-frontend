5f8f6981c016218853a23fd9feaa52765c2314f3
Orlando Carnate <orlando.carnate@gmail.com>
2021-07-10 15:52:33 -0500

    update HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
-import logo from './upssf-logo.svg';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import logo from "./upssf-logo.svg";
 
 class Home extends Component {
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header upssf-cover">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Fighting Poverty Through Education
-                    </p>
-                    <p>We are currenlty updating our site!</p>
-                    <p>
-                        You can email us at <a className="Home-link" href="mailto:info@upssf.org">info@upssf.org</a>
-                    </p>
-                </header>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header upssf-cover">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Fighting Poverty Through Education</p>
+          <p>
+            Welcome to the University of the Philippines SILAK-SILAB Foundation,
+            a non-profit, tax-exempt Section 501(c)(3) public charity
+            organization incorporated in Illinois, USA. It believes in the value
+            of education and is dedicated to improving the lives of
+            disadvantaged young individuals by providing educational assistance
+            such as scholarships, tuition assistance, and stipends, for
+            qualified students. The Foundation’s mission is to liberate poor but
+            deserving students from poverty through access to educatio
+          </p>
+          <p>
+            Contact us at{" "}
+            <a className="Home-link" href="mailto:info@upssf.org">
+              info@upssf.org
+            </a>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
